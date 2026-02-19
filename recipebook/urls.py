86304bdd895fa9099bfb4b235df0d229fe5fe25a
FaithPalencia <kristin.palencia@student.ateneo.edu>
2026-02-20 07:39:17 +0800

    Removed app_name.

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -2,7 +2,6 @@
 from django.contrib import admin
 from django.urls import include, path
 
-app_name = 'ledger'
 urlpatterns = [
     path('', include('ledger.urls')),
     path('admin/', admin.site.urls),
