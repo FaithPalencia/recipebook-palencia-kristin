000336a9590473dbeb356ba16760141c64299425
FaithPalencia <kristin.palencia@student.ateneo.edu>
2026-02-20 02:49:50 +0800

    Added Recipe model and name field

--- ledger/models.py
+++ ledger/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
