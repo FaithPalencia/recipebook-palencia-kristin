4627318b6706584b941e0add0280a5fbefe96c70
FaithPalencia <kristin.palencia@student.ateneo.edu>
2026-03-03 00:14:00 +0800

    Changed author foreign key related name

--- ledger/models.py
+++ ledger/models.py
@@ -24,7 +24,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
-        related_name='recipe')
+        related_name='recipes')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
