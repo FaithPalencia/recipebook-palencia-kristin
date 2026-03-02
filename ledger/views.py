96b4e1c55a3cded65f2f40f9e82ad575c6e6990e
FaithPalencia <kristin.palencia@student.ateneo.edu>
2026-03-03 00:59:23 +0800

    Added login requirement

--- ledger/views.py
+++ ledger/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 
 # Create your views here.
@@ -6,6 +7,7 @@ def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request,'ledger/recipe_list.html',{'recipes':recipes})
 
+@login_required()
 def recipe_detail(request,id):
     recipe = Recipe.objects.get(id=id)
     return render(request, 'ledger/recipe_ingredients.html',{'recipe':recipe})
