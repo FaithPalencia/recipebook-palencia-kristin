6e8a642d0f51aa0e1968c0d09dcca3ada4cdfd31
FaithPalencia <kristin.palencia@student.ateneo.edu>
2026-03-02 23:17:39 +0800

    Added bio field

--- ledger/models.py
+++ ledger/models.py
@@ -39,3 +39,4 @@ class RecipeIngredient(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True)
