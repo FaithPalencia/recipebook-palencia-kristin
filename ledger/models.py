19e8d0a2dcce2980b243b9abe882cc0190ce613d
FaithPalencia <kristin.palencia@student.ateneo.edu>
2026-03-02 23:12:03 +0800

    Imported User in to models.py

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Ingredient(models.Model):
@@ -36,4 +37,4 @@ class RecipeIngredient(models.Model):
     )
 
 class Profile(models.Model):
-    user = models.OneToOneField(user,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
