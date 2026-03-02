89227d7d2be34c6ef490ce5cbf4429f34fab0226
FaithPalencia <kristin.palencia@student.ateneo.edu>
2026-03-02 23:02:44 +0800

    Fixed incorrect URL pointer for Ingredient model

--- ledger/models.py
+++ ledger/models.py
@@ -9,7 +9,7 @@ class Ingredient(models.Model):
         return self.name
     
     def get_absolute_url(self):
-        return reverse('recipe_list')
+        return reverse('ingredient_detail',args=[self.id])
 
 
 class Recipe(models.Model):
