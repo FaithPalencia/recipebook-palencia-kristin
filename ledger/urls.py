7da4cf345e85c04d8a3de51dbebd7c5d8d76d737
FaithPalencia <kristin.palencia@student.ateneo.edu>
2026-03-13 02:12:40 +0800

    Fixed issues from Lab3

--- ledger/urls.py
+++ ledger/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
-    path('recipe/<int:id>', views.recipe_detail, name='recipe_detail'),
+    path('recipe/<int:id>/', views.recipe_detail, name='recipe_detail'),
 ]
