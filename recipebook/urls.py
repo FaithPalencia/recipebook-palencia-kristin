1ec3b48f97ebb716121aa125e0d868b817488fe1
FaithPalencia <kristin.palencia@student.ateneo.edu>
2026-03-13 16:31:33 +0800

    Adding images through admin.

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -1,6 +1,8 @@
 # project/urls.py
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -8,3 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
