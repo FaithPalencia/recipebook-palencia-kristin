76aec0d8c6870f655358fb83c90cedd26641b0b9
FaithPalencia <kristin.palencia@student.ateneo.edu>
2026-03-13 16:25:39 +0800

    Modified Recipe admin to contain RecipeImage models related to it.

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import Recipe, Profile, Ingredient, RecipeIngredient
+from .models import Recipe, Profile, RecipeIngredient, RecipeImage, Ingredient
 
 # Register your models here.
-admin.site.register(Recipe)
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeImageInline, RecipeIngredientInline,]
+
 admin.site.register(Profile)
 admin.site.register(Ingredient)
 admin.site.register(RecipeIngredient)
+admin.site.register(RecipeImage)
+admin.site.register(Recipe, RecipeAdmin)
